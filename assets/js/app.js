787f4f0f70ffb535fcf92ed54119dc3719f9b850
Noah2211 <noah.decroix3@gmail.com>
2022-01-21 15:55:32 +0100

    Minor changes/upgrade

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,18 @@
 const begin = document.getElementById('begin');
-begin.innerHTML = "Veuillez entrez un nom de ville dans le champ de recherche en haut à droite";
-let city;
 const inputCity = document.getElementById('searchCity');
+const titleCity = document.getElementById('titleCity');
+const temp = document.getElementById('temp');
+const maxTemp = document.getElementById("maxTemp");
+const minTemp = document.getElementById("minTemp");
+const feel = document.getElementById('feel');
+const pressure = document.getElementById('pressure');
+const cielP = document.getElementById('ciel');
+const speedWind = document.getElementById('windSpeed');
+const cityInfo = document.getElementById('cityInfo');
+let city;
+
 inputCity.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-        let cityInfo = document.getElementById('cityInfo');
         cityInfo.style.height = "auto";
         begin.style.display = "none";
         city = document.getElementById('searchCity').value;
@@ -14,56 +22,45 @@ inputCity.addEventListener('keypress', (e) => {
 
             .then(response => response.json())
             .then(response => {
-                console.log(response)
-                //response.main = an objet of API, this object contane temp_max, temp_min, temp, feels_like, pressure.
-                let temperature = response.main;
+                console.log(response);
+                //response.main = an objet of API, this object contain temp_max, temp_min, temp, feels_like, pressure.
+                const temperature = response.main;
 
                 //response.weather contain the clarity of the sky.
-                let sky = response.weather;
+                const sky = response.weather;
 
                 //response.wind contain the speed of the wind.
-                let wind = response.wind;
+                const wind = response.wind;
 
-                let titleCity = document.getElementById('titleCity');
                 titleCity.innerHTML = `${city[0].toUpperCase()}${city.slice(1)}:`;
+                titleCity.style.color = "white";
 
                 //Actual temperature for each city specified on the input.
-                let temp = document.getElementById('temp');
                 temp.innerHTML = `Température: ${Math.trunc(temperature.temp)} °C`;
 
                 //Max temperature of the day.
-                let maxTemp = document.getElementById("maxTemp");
                 maxTemp.innerHTML = `Température maximale: ${Math.trunc(temperature.temp_max)} °C`;
 
                 //Min temperature of the day.
-                let minTemp = document.getElementById("minTemp");
                 minTemp.innerHTML = `Température minimale: ${Math.trunc(temperature.temp_min)} °C`;
 
                 //The temperature feels like.
-                let feel = document.getElementById('feel');
                 feel.innerHTML = `Ressenti: ${Math.trunc(temperature.feels_like)} °C `;
 
                 //The pressure of the day.
-                let pressure = document.getElementById('pressure');
                 pressure.innerHTML = `Pression atmosphérique: ${temperature.pressure} hpa`;
 
                 //Clarity of the sky.
-                let cielP = document.getElementById('ciel');
                 cielP.innerHTML = `Ciel: ${sky[0].description}`
 
-                let speedWind = document.getElementById('windSpeed');
                 speedWind.innerHTML = `Vitesse du vent: ${wind.speed} km/h`;
-                titleCity.style.color = "white";
+
 
             })
             .catch(() => {
-                let title = document.getElementById('titleCity');
-                title.style.color = "red";
-                title.innerHTML = "Veuillez saisir un nom de ville valide.";
+                titleCity.style.color = "red";
+                titleCity.innerHTML = "Veuillez saisir un nom de ville valide.";
             });
     }
 
-
 })
-
-
